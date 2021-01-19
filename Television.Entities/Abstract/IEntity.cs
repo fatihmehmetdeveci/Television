cfc7a5bae529b473472645760eae3b5ac1514932
Fatih Mehmet Deveci <fatihmhmtdeveci@outlook.com>
2021-01-19 22:59:52 +0300

    Entitys

---
+++ Television.Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Television.Entities.Abstract
+{
+    public interface IEntity
+    {
+    }
+}
---
+++ Television.Entities/Concrete/Television.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Television.Entities.Abstract;
+
+namespace Television.Entities.Concrete
+{
+    public class Television :IEntity
+    {
+        public int TvID { get; set; }
+        public string Name { get; set; }
+        public double Frequency { get; set; }
+        public bool IsNewsChannel { get; set; }
+        public Television()
+        {
+
+        }
+
+        public Television(string name, double frequency, bool isNewsChannel)
+        {
+            Name = name;
+            Frequency = frequency;
+            IsNewsChannel = isNewsChannel;
+        }
+
+        public override string ToString()
+        {
+            return $"{TvID,-5}{Name,-20}{Frequency,-10}{IsNewsChannel,-10}";
+        }
+    }
+}
